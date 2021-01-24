d5793e2e76a67d7c2d3ef52224eba485022c9410
XXLZopes <afordm99@gmail.com>
2021-01-24 12:35:53 -0800

    many to many

--- models/index.js
+++ models/index.js
@@ -19,9 +19,16 @@ Product.belongsToMany(Tag, {
   through: ProductTag,
   as: 'tagged_product',
   foreignKey: 'product_id'
-})
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tagged_product',
+  foreignKey: 'tag_id'
+});
+
+
 
 module.exports = {
   Product,
